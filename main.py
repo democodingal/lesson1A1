a2c00d6ab3f0fde2a234df7b025153470c12049e
democodingal <mayurirastogi0@gmail.com>
2024-10-22 21:33:10 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+#simple printing
 print(" hello codingal")
+
+# printing my name
+print(" my name is mayuri")
+
+#printing a fun fact about me
+print("i love reading books")
+
+#performing and printing a simple calculation
+print("the result of 3+5 is = ", 3+5)
